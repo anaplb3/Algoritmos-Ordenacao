6729a1e06d9f042245a9a4faf32d78eff159713e
Ana Paula de Lima Borges Silva <ana.paula@DCX.UFPB.BR>
2018-09-25 13:05:38 -0300

    não tá funcionando ainda

---
+++ QuickSort.java
@@ -0,0 +1,50 @@
+package algoritmos;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+
+		int[] v = { 5, 2, 1, 4, 3 };
+		quicksort(v, 0, v.length - 1);
+		System.out.println(Arrays.toString(v));
+	}
+
+	private static void quicksort(int[] v, int esq, int dir) {
+		if (esq < dir) {
+			int pivo = particionar(v, esq, dir);
+			quicksort(v, esq, pivo - 1);
+			quicksort(v, pivo + 1, dir);
+		}
+
+	}
+
+	private static int particionar(int[] v, int esq, int dir) {
+		int pivo = esq;
+		int i = esq + 1;
+		int j = dir;
+
+		while (i <= j) {
+			if (v[i] <= pivo) {
+				i++;
+			} else if (v[j] > pivo) {
+				j--;
+			} else if (i <= j) {
+				swap(v, i, j);
+				i++;
+				j--;
+			}
+		}
+		swap(v, esq, j);
+
+		return j;
+	}
+
+	private static void swap(int[] v, int i, int j) {
+		int aux = v[i];
+		v[i] = v[j];
+		v[j] = aux;
+	}
+
+}
